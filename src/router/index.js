c04cdacc59e17302eb629d16c39397cee1c5b33c
ismailkurt <ismail.kurt7749@gmail.com>
2024-02-22 11:56:23 +0300

    Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
+import NotFound from '../views/NotFound.vue'
+import Jobs from '../views/Jobs/Jobs.vue'
+import JobDetails from '../views/Jobs/JobDetails.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  //redirect
+  {
+    path: '/all-jobs',
+    redirect: 'jobs'
+  },
+  //catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod corrupti temporibus, tempora assumenda enim dolorem magnam optio molestias ab, magni minima animi neque. Quos laborum nam sunt, laboriosam facere minus blanditiis quia illum rem aliquam, id ad asperiores, nemo numquam itaque ipsa culpa minima maiores architecto dolor nobis consequuntur consequatur? Accusantium unde deleniti hic amet officiis, exercitationem quos repudiandae fugiat maxime maiores quidem nobis natus cupiditate inventore nihil. Quam provident eveniet repudiandae eum tempora maxime quidem natus. Inventore doloremque culpa praesentium repudiandae dignissimos neque dolor incidunt ipsum modi at sit architecto, qui totam cum explicabo, tempora non laboriosam suscipit quia!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod corrupti temporibus, tempora assumenda enim dolorem magnam optio molestias ab, magni minima animi neque. Quos laborum nam sunt, laboriosam facere minus blanditiis quia illum rem aliquam, id ad asperiores, nemo numquam itaque ipsa culpa minima maiores architecto dolor nobis consequuntur consequatur? Accusantium unde deleniti hic amet officiis, exercitationem quos repudiandae fugiat maxime maiores quidem nobis natus cupiditate inventore nihil. Quam provident eveniet repudiandae eum tempora maxime quidem natus. Inventore doloremque culpa praesentium repudiandae dignissimos neque dolor incidunt ipsum modi at sit architecto, qui totam cum explicabo, tempora non laboriosam suscipit quia!</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
